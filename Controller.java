0eb92cd343cd6723178f57322843fbe3fc3a85ea
victor <victor_cardoso00@hotmail.com>
2019-07-22 15:40:53 -0300

    working evertyghin

--- Controller.java
+++ Controller.java
@@ -1,9 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import mips.Instruction;
 import mips.Processor;
@@ -17,27 +13,44 @@ public class Controller {
 	private volatile boolean running = false;
 	private boolean hexadecimal = false;
 
-	public Controller() {
+	public Controller () {
 		processor = new Processor();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println("Entre com o nome do arquivo texto:");
+			String arquivo = reader.readLine();
+			arquivo = "teste.txt";
+			load(arquivo);
+		} catch (IOException e) {}
+
+        do {
+			try {
+        		reader.readLine();
+			} catch (IOException e) {}
+			step();
+        } while (1==1);
 	}
 	private void refresh() {
 		int pc = processor.getPcValue();
 
-		int instructionIndex = pc/4;
-		
 		int[] registerData = processor.getRegisters();
 		List<Integer> changedRegisters = processor.getChangedRegisters();
+		System.out.println("====================[ REGISTRADORES ]====================");
 		for(int index : changedRegisters) {
 			String repr = String.format(
 					"%s: %s", RegisterFile.name(index), string_value(registerData[index]));
+			System.out.println(repr);
 		}
 
 		int[] memoryData = processor.getMemory();
 		List<Integer> changedMemory = processor.getChangedMemory();
+		System.out.println("\n====================[    MEMORIA    ]====================");
 		for(int index : changedMemory) {
 			String repr = String.format(
 					"%s: %s", string_value((short)index), string_value(memoryData[index]));
+			System.out.println(repr);
 		}
+		System.out.println("\n--------------------------------------------------------------------------");
 	}
 
 	private String string_value(int b) {
@@ -50,6 +63,7 @@ public class Controller {
 
 	private synchronized void step() {
 		processor.step();
+		refresh();
 	}
 
 	private void load(String filename) {
@@ -88,6 +102,5 @@ public class Controller {
 
 	public static void main(String[] args) {
 		new Controller();
-		System.out.println("asdoi");
 	}
 }
